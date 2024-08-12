5455089e85ffa365a08f702c92946672065834d7
Roshan Basnet <broshan531@gmail.com>
2024-08-13 05:06:47 +0545

    Modified directory path in settings.py

--- JobRecommendationSystem/JobRecommendationSystem/urls.py
+++ JobRecommendationSystem/JobRecommendationSystem/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
 
 from django.urls import path
 from . import views
